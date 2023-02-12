ebd41733cc1acb07ce021e2a813f84fce391dd1a
Ra Wai Aung <rawaiucsm@gmail.com>
2023-02-12 23:42:24 +0630

    add album

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,5 +1,36 @@
-function AlbumsList({user}) {
-    return <div>Albums for {user.name}</div>
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
+import Skeleton from "./Skeleton";
+
+function AlbumsList({ user }) {
+    const { data, error, isLoading } = useFetchAlbumsQuery(user);
+    const [ addAlbum, results] = useAddAlbumMutation();
+    const handleAddAlbum = () => {
+        addAlbum(user);
+    }
+    let content;
+    if (isLoading) {
+        content = <Skeleton />
+    } else if (error) {
+        content = <div>Error loading albums.</div>
+    } else {
+        content = data.map(album => {
+            const header = <div>{album.title}</div>
+            return <ExpandablePanel key={album.id} header={header}>
+                List of  photos in the album
+            </ExpandablePanel>
+        })
+    }
+    return <div>
+            <div>
+                Albums for {user.name}
+                <Button onClick={handleAddAlbum}>
+                    + Add Album
+                </Button>
+            </div>
+            <div>{content}</div>
+        </div>
 }
 
 export default AlbumsList;
